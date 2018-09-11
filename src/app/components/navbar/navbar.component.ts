621b7648e428c86215fda7e0c750cb98d2990218
Javier Lozano <javier_l_o@hotmail.com>
2018-09-10 22:01:19 -0500

    Se crea servicio y componentes para el ejemplo

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.auth.login();
+  }
+
+}
